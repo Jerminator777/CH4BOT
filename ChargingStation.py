92256410da7887a87d50f91c76cf33a77d9187af
PYTHONISMYFRIEND <37674350+PYTHONISMYFRIEND@users.noreply.github.com>
2018-03-26 12:38:10 -0400

    Update ChargingStation.py

--- ChargingStation.py
+++ ChargingStation.py
@@ -3,3 +3,17 @@ class ChargingStation(Entity.Entity):
 	def __init__(self, xposition, yposition):
 		Entity.Entity.__init__(self, [xposition, yposition, 25, 25], "c")
 		self.status = "Available"
+		
+	def Send_Availability(self):
+		if self.status == "Available":
+			print([self.xposition, self.yposition])	
+		
+	def Receive_Signal(self, signal_type):
+		#signal_type is ["command" , [xpos_station, ypos_station]]
+		if signal_type[0] == "Request":
+			self.Send_Availability()
+		elif signal_type[0] == "Selection":
+		    [xsel, ysel] = signal_type[1]
+			if [xsel, ysel] == [self.xposition, self.yposition]:		
+				self.status = "Reserved"
+				print("Reserved")	
