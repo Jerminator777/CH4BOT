61a9f2dd3e116ae15782d0881c66e1b668c8ac19
PYTHONISMYFRIEND <37674350+PYTHONISMYFRIEND@users.noreply.github.com>
2018-04-02 21:42:16 -0400

    Add files via upload

---
+++ Interface.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy
+import pygame
+
+
+#SECTION FOR TURNING LANDFILL MATRIX INTO .PNG FILE TO BE USED AS BACKGROUND OF PYTHON WINDOW
+WINDOW
+#Assuming landfill "map" comes in lists of binary lists
+landfill_temp = [[0,0,0,0,0,0,0,0,0],
+				 [0,0,1,1,1,0,1,1,0],
+				 [0,0,1,1,1,1,1,1,0],
+				 [0,0,0,0,0,0,1,1,0],
+				 [0,0,0,0,0,0,0,0,0]]
+				 
+#invert colours (black=1,white=0)
+landfill_invert	= numpy.logical_not(landfill_temp).astype(int)
+
+#save image. small white border remains.
+fig = plt.imshow(landfill_invert, cmap='Greys')
+plt.axis('off')
+fig.axes.get_xaxis().set_visible(False)
+fig.axes.get_yaxis().set_visible(False)
+plt.savefig('LandfillMap.png',bbox_inches='tight')
+
+
+#SECTION FOR CREATING PYTHON WINDOW
+#initializing window screen, screen background, and refresh time
+clock = pygame.time.Clock()
+background = pygame.image.load("LandfillMap.png")
+background_size = background.get_rect().size
+screen = pygame.display.set_mode(background_size)
+pygame.display.set_caption("Simulation Window")
+
+#loop for refreshing window
+while True:	
+	clock.tick(10) #controls how fast screen updates
+	screen.blit(background, (0,0))
+	pygame.display.flip()
